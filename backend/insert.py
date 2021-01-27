119f4d8752f53b59d0ca41cdbc0f330297ce7d01
Ivan Kamas <kamasivo@gmail.com>
2021-01-27 17:08:30 +0100

    insert do db z pythona ide

---
+++ backend/insert.py
@@ -0,0 +1,32 @@
+import psycopg2
+from config import config
+
+
+def insert(ip, os, name, vulns):
+    """ insert a new vendor into the vendors table """
+    sql = """INSERT INTO devices(ip_address, os, name, num_of_vulns)
+             VALUES(%s, %s, %s, %s) RETURNING id;"""
+    conn = None
+    id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (ip, os, name, vulns,))
+        # get the generated id back
+        id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return id
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,32 @@
+import nmap
+from connect import connect
+from insert import insert
+
+
 print("hello world")
+
+connect()   #check connection
+
+insert("192.168.0.1", "WIN 7", "Jogurt", 1)
+
+# initialize the port scanner
+# nmScan = nmap.PortScanner()
+
+# scan localhost for ports in range 21-443
+# nmScan.scan('127.0.0.1')
+# scanRange = nmScan.scan(hosts="192.168.0.0/24")
+# print(scanRange)
+
+# print(nmScan.all_hosts())
+
+# # run a loop to print all the found result about the ports
+# for host in nmScan.all_hosts():
+#      print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+#      print('State : %s' % nmScan[host].state())
+#      for proto in nmScan[host].all_protocols():
+#         print('----------')
+#         print('Protocol : %s' % proto)
+ 
+#         lport = nmScan[host][proto].keys()
+
+
